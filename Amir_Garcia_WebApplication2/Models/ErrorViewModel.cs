061047c502e46c6d5873c7e358595dcf7774d123
amir_ <amir_@ASUSAG0201>
2024-04-08 10:28:09 -0500

    Agregar archivos de proyecto.

---
+++ Amir_Garcia_WebApplication2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Amir_Garcia_WebApplication2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
